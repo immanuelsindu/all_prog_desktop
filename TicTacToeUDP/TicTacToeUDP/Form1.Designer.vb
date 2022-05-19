abbe7962dc749d04a109424f2db67948fde5cc1e
ImmanuelSindu <immanuel.sindu@ti.ukdw.ac.id>
2022-05-19 22:23:17 +0700

    fix semua

--- TicTacToeUDP/TicTacToeUDP/Form1.Designer.vb
+++ TicTacToeUDP/TicTacToeUDP/Form1.Designer.vb
@@ -40,6 +40,8 @@ Partial Class Form1
         Me.Button8 = New System.Windows.Forms.Button()
         Me.Button9 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnPlayAgain = New System.Windows.Forms.Button()
+        Me.lblHighScore = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -202,15 +204,35 @@ Partial Class Form1
         Me.Label3.AutoSize = True
         Me.Label3.Location = New System.Drawing.Point(12, 346)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
         Me.Label3.TabIndex = 0
-        Me.Label3.Text = "High Score"
+        Me.Label3.Text = "High Score : "
+        '
+        'btnPlayAgain
+        '
+        Me.btnPlayAgain.Location = New System.Drawing.Point(224, 341)
+        Me.btnPlayAgain.Name = "btnPlayAgain"
+        Me.btnPlayAgain.Size = New System.Drawing.Size(68, 23)
+        Me.btnPlayAgain.TabIndex = 13
+        Me.btnPlayAgain.Text = "Play Again"
+        Me.btnPlayAgain.UseVisualStyleBackColor = True
+        '
+        'lblHighScore
+        '
+        Me.lblHighScore.AutoSize = True
+        Me.lblHighScore.Location = New System.Drawing.Point(76, 347)
+        Me.lblHighScore.Name = "lblHighScore"
+        Me.lblHighScore.Size = New System.Drawing.Size(13, 13)
+        Me.lblHighScore.TabIndex = 14
+        Me.lblHighScore.Text = "0"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(304, 371)
+        Me.Controls.Add(Me.lblHighScore)
+        Me.Controls.Add(Me.btnPlayAgain)
         Me.Controls.Add(Me.Button9)
         Me.Controls.Add(Me.Button8)
         Me.Controls.Add(Me.Button7)
@@ -252,4 +274,6 @@ Partial Class Form1
     Friend WithEvents Button8 As Button
     Friend WithEvents Button9 As Button
     Friend WithEvents Label3 As Label
+    Friend WithEvents btnPlayAgain As Button
+    Friend WithEvents lblHighScore As Label
 End Class
--- TicTacToeUDP/TicTacToeUDP/Form1.vb
+++ TicTacToeUDP/TicTacToeUDP/Form1.vb
@@ -4,6 +4,121 @@ Imports System.Text
 Imports System.Threading
 Public Class Form1
     Dim isClient As Boolean = False
+    'setiap kali menang tambah 10 poin
+    Dim highScore As Integer = 0
+
+    'untuk mengecek apakah sudah menang atau belum
+    'pengecekan dilakukan sesuai pola yang memungkinkan terjadi kemenangan 
+    Private Sub checkWin()
+        If (Button1.Text IsNot "") And (Button1.Text = Button2.Text) And (Button2.Text = Button3.Text) Then
+            highScore += 10
+            If Button1.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+        ElseIf (Button4.Text IsNot "") And (Button4.Text = Button5.Text) And (Button5.Text = Button6.Text) Then
+            highScore += 10
+            If Button4.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+        ElseIf (Button7.Text IsNot "") And (Button7.Text = Button8.Text) And (Button8.Text = Button9.Text) Then
+            highScore += 10
+            If Button7.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+        ElseIf (Button1.Text IsNot "") And (Button1.Text = Button4.Text) And (Button4.Text = Button7.Text) Then
+            highScore += 10
+            If Button1.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+        ElseIf (Button2.Text IsNot "") And (Button2.Text = Button5.Text) And (Button5.Text = Button8.Text) Then
+            highScore += 10
+            If Button2.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+        ElseIf (Button3.Text IsNot "") And (Button3.Text = Button6.Text) And (Button6.Text = Button9.Text) Then
+            highScore += 10
+            If Button3.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+        ElseIf (Button1.Text IsNot "") And (Button1.Text = Button5.Text) And (Button5.Text = Button9.Text) Then
+            highScore += 10
+            If Button1.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+        ElseIf (Button3.Text IsNot "") And (Button3.Text = Button5.Text) And (Button5.Text = Button7.Text) Then
+            highScore += 10
+            If Button3.Text = "X" Then
+                MsgBox("Player 1 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            Else
+                MsgBox("Player 2 Menang !!", MessageBoxButtons.OK, "Kamu Menang")
+                lblHighScore.Text = CStr(highScore)
+            End If
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+
+            'jika semua pola di lewati berarti mungkin draw
+        ElseIf checkDraw() Then
+            MsgBox("Hasil Seimbang !!", MessageBoxButtons.OK, "Seimbang")
+            MsgBox("Klik masing-masing tombol Play Again untuk bermain lagi", MessageBoxButtons.OK, "Main Lagi")
+        End If
+
+    End Sub
+
+    Private Function checkDraw() As Boolean
+        Dim b() As Button = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
+        Dim cc As Integer = 0
+        For i As Byte = 0 To 8
+            If b(i).Text IsNot "" Then
+                cc += 1
+            End If
+        Next i
+        If cc = 9 Then
+            Return True
+        End If
+        Return False
+    End Function
 
     Private Sub checkIfButtonDisabled()
         Dim b() As Button = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
@@ -135,9 +250,10 @@ Public Class Form1
                 checkIfButtonDisabled()
                 lblStatus.Text = "Giliran kamu"
         End Select
-        'lbHasil.Items.Add(teks)
-
     End Sub
+
+
+    'untuk observer di sisi server
     Public Sub serverThread()
         Dim udpClient As New UdpClient(8083)
         While True
@@ -150,6 +266,7 @@ Public Class Form1
         End While
     End Sub
 
+    'untuk observer di sisi client
     Public Sub serverThread2()
         Dim udpClient As New UdpClient(8084)
         While True
@@ -168,13 +285,10 @@ Public Class Form1
 
         Dim thdUDPServer = New Thread(New ThreadStart(AddressOf serverThread))
         thdUDPServer.Start()
-
-
     End Sub
 
     Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
         lblSebagai.Text = "Sebagai : Server (Player 1)"
-
         isClient = True
         enableAllButton()
         lblStatus.Text = "Giliran Kamu"
@@ -184,7 +298,6 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
         If (isClient = True) Then
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8083)
@@ -195,6 +308,8 @@ Public Class Form1
             disableAllButton()
             Button1.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            'cek apakah menang atau tidak
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -205,6 +320,7 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
     End Sub
 
@@ -220,6 +336,7 @@ Public Class Form1
             disableAllButton()
             Button2.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Button2.Text = "O"
             disableAllButton()
@@ -231,6 +348,7 @@ Public Class Form1
             Dim sendBytes As Byte()
             sendBytes = Encoding.ASCII.GetBytes("2")
             udpClient.Send(sendBytes, sendBytes.Length)
+            checkWin()
 
         End If
     End Sub
@@ -246,6 +364,7 @@ Public Class Form1
             disableAllButton()
             Button3.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -256,6 +375,7 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
     End Sub
 
@@ -270,6 +390,7 @@ Public Class Form1
             disableAllButton()
             Button4.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -280,6 +401,7 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
     End Sub
 
@@ -294,6 +416,7 @@ Public Class Form1
             disableAllButton()
             Button5.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -304,6 +427,7 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
     End Sub
 
@@ -318,6 +442,7 @@ Public Class Form1
             disableAllButton()
             Button6.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -328,6 +453,7 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
     End Sub
 
@@ -342,6 +468,7 @@ Public Class Form1
             disableAllButton()
             Button7.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -352,6 +479,7 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
     End Sub
 
@@ -366,6 +494,7 @@ Public Class Form1
             disableAllButton()
             Button8.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -376,6 +505,7 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
     End Sub
 
@@ -390,6 +520,7 @@ Public Class Form1
             disableAllButton()
             Button9.Text = "X"
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         Else
             Dim udpClient As New UdpClient
             udpClient.Connect(txtIP.Text, 8084)
@@ -400,6 +531,16 @@ Public Class Form1
             disableAllButton()
 
             lblStatus.Text = "Giliran Musuh"
+            checkWin()
         End If
+    End Sub
+
+    Private Sub btnPlayAgain_Click(sender As Object, e As EventArgs) Handles btnPlayAgain.Click
+        Dim b() As Button = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
+
+        For i As Byte = 0 To 8
+            b(i).Enabled = True
+            b(i).Text = ""
+        Next i
     End Sub
 End Class
